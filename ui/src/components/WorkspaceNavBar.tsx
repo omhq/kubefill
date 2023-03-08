a23ff8e707dc650846b375166b94f51bb8809994
Samuel Rowe <samuelrowe1999@gmail.com>
2023-03-08 20:51:11 +0530

    feat(ui): created `WorkspaceNavBar` component

---
+++ ui/src/components/WorkspaceNavBar.tsx
@@ -0,0 +1,27 @@
+import { darken, AppBar, Toolbar, styled } from "@mui/material";
+import { FunctionComponent, ReactElement, ReactNode } from "react";
+
+const StyledAppBar = styled(AppBar)`
+  background-color: ${({ theme }) => darken(theme.palette.background.paper, 0.03)};
+  color: ${({ theme }) => theme.palette.getContrastText(theme.palette.background.paper)};
+`;
+
+const StyledToolbar = styled(Toolbar)`
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+export interface IWorkspaceNavBarProps {
+  children?: ReactNode;
+}
+
+export const WorkspaceNavBar: FunctionComponent<IWorkspaceNavBarProps> = (
+  props: IWorkspaceNavBarProps
+): ReactElement => {
+  const { children } = props;
+  return (
+    <StyledAppBar position="static" elevation={0}>
+      <StyledToolbar>{children}</StyledToolbar>
+    </StyledAppBar>
+  );
+};
