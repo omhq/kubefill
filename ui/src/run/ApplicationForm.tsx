b9f9651bfa40f2cc15255f8bbc64d92f83306a26
Samuel Rowe <samuelrowe1999@gmail.com>
2023-03-18 00:20:14 +0530

    feat(ui): updated `ApplicationForm` to use custom templates and widgets

--- ui/src/run/ApplicationForm.tsx
+++ ui/src/run/ApplicationForm.tsx
@@ -1,26 +1,31 @@
 import { IChangeEvent } from "@rjsf/core";
 import Form from "@rjsf/mui";
 import validator from "@rjsf/validator-ajv8";
-import { useState } from "react";
-import { TextField } from "../components";
+import { useCallback, useState } from "react";
 import { FormData } from "../types";
+import { getTemplates, getWidgets } from "../rjsf";
 
-const ApplicationForm = ({
-  defaultData,
-  schema,
-  uiSchema,
-  handleFormChange,
-}: {
+export interface IApplicationForm {
   defaultData: any;
   schema: any;
   uiSchema: any;
   handleFormChange: (data: FormData) => void;
-}) => {
+}
+
+const templates = getTemplates();
+const widgets = getWidgets();
+
+const ApplicationForm = (props: IApplicationForm) => {
+  const { defaultData, schema, uiSchema, handleFormChange } = props;
   const [data, setData] = useState<FormData>(defaultData);
-  const handleOnChange = (data: IChangeEvent) => {
-    setData(data.formData);
-    handleFormChange(data.formData);
-  };
+
+  const handleOnChange = useCallback(
+    (data: IChangeEvent) => {
+      setData(data.formData);
+      handleFormChange(data.formData);
+    },
+    [handleFormChange]
+  );
 
   const log = (type: string) => console.log.bind(console, type);
 
@@ -34,7 +39,8 @@ const ApplicationForm = ({
       onError={log("errors")}
       showErrorList={false}
       liveValidate
-      widgets={{ TextWidget: TextField as any }}
+      widgets={widgets}
+      templates={templates}
     />
   );
 };
