7398103aa458437f7ab2eae9dab57776377b16ec
Samuel Rowe <samuelrowe1999@gmail.com>
2023-05-03 08:05:32 +0530

    feat(ui): updated application form to sanitize env vars to hide secrets

--- ui/src/run/ApplicationForm.tsx
+++ ui/src/run/ApplicationForm.tsx
@@ -1,7 +1,7 @@
 import { IChangeEvent } from "@rjsf/core";
 import Form from "@rjsf/mui";
 import validator from "@rjsf/validator-ajv8";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { FormData } from "../types";
 import { getTemplates, getWidgets } from "../rjsf";
 import { Box } from "@mui/material";
@@ -30,12 +30,40 @@ const ApplicationForm = (props: IApplicationForm) => {
 
   const log = (type: string) => console.log.bind(console, type);
 
+  const sanitizedData = useMemo(() => {
+    if (!props.defaultData.spec.template.spec.containers) {
+      return props.defaultData;
+    }
+    // TODO: Don't directly update the received object!
+    props.defaultData.spec.template.spec.containers =
+      props.defaultData.spec.template.spec.containers.map((container: any) => ({
+        ...container,
+        env: container.env.map((item: any) => {
+          if ("value" in item) {
+            if (/{{secrets\.[a-zA-Z_][a-zA-Z0-9_]*}}/.test(item.value)) {
+              return {
+                name: item.name,
+                value: "******",
+              };
+            }
+            return item;
+          }
+          /* valueFrom should be sanitized. */
+          return {
+            name: item.name,
+            value: "******",
+          };
+        }),
+      }));
+    return props.defaultData;
+  }, [props.defaultData]);
+
   return (
     <Box mt={2}>
       <Form
         schema={schema}
         uiSchema={uiSchema}
-        formData={data}
+        formData={sanitizedData}
         validator={validator}
         onChange={handleOnChange}
         onError={log("errors")}
